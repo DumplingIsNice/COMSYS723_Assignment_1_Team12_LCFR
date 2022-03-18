65c934858a6522f80cbdd5c9232fcce21385faaf
Hao Lin <68680028+uoa-hlin784@users.noreply.github.com>
2022-03-18 18:59:50 +1300

    Added LED API

---
+++ software/customAPI/led.h
@@ -0,0 +1,61 @@
+/*
+ * leds.h
+ *
+ *  Created on: 18 Mar 2022
+ *      Author: Hao Lin
+ */
+
+#ifndef LEDS_H_
+#define LEDS_H_
+
+#include "project.h"
+
+/* LED Peripheral Values */
+
+// LED Binary Positions
+#define LED_0 		(1 << 0)
+#define LED_1 		(1 << 1)
+#define LED_2 		(1 << 2)
+#define LED_3 		(1 << 3)
+#define LED_4 		(1 << 4)
+#define LED_5 		(1 << 5)
+#define LED_6 		(1 << 6)
+#define LED_7 		(1 << 7)
+
+#define LED_G_ALL 	0xff
+#define LED_R_ALL 	0x3ffff
+
+// LED Colour Macro
+#define LED_RED		'r'
+#define LED_GREEN	'g'
+
+// Nios II LED API Macros
+#define GET_LED_RED 		IORD_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE)
+#define GET_LED_GREEN		IORD_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE)
+
+#define SET_LED_RED(x) 		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, x)
+#define SET_LED_GREEN(x) 	IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, x)
+
+#define CLEAR_LED_RED 		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, 0)
+#define CLEAR_LED_GREEN 	IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0)
+
+/* Function Declarations */
+
+// Sets up LED initial status
+void led_init();
+
+// Writes value of LED colour by position
+	/*
+	 * Inputs:
+	 * color of led - Macro
+	 * binary position of led - logic high, rest logic low
+	 * desired on or off - Macro
+	*/
+void led_write(char colour, uint led_pos, uint is_on);
+
+/* Helper Functions */
+
+// Returns led_pos merged with current position, ready to be write to led port
+uint set_on_off(uint led_pos, uint current_led_pos, uint is_on);
+
+#endif /* LEDS_H_ */
---
+++ software/customAPI/project.h
@@ -0,0 +1,48 @@
+/*
+ * project.h
+ *
+ *  Created on: 18 Mar 2022
+ *      Author: Hao Lin
+ */
+
+// Contains Common Declarations
+#ifndef PROJECT_H_
+#define PROJECT_H_
+
+// Standard includes
+#include <stddef.h>
+// Standard I/O
+#include <stdio.h>
+// String Functions
+#include <string.h>
+
+// CPU Generated Symbolic Names
+#include <system.h>
+// Interrupt library
+#include <sys/alt_irq.h>
+// PIO register writing and reading operation
+#include <altera_avalon_pio_regs.h>
+
+// Delay functions
+// #include <unistd.h>
+
+// Timer
+#include <sys/alt_alarm.h>
+
+// Scheduler includes
+//#include "freertos/FreeRTOS.h"
+//#include "freertos/task.h"
+//#include "freertos/queue.h"
+//#include "freertos/semphr.h"
+
+// Custom API Libraries
+
+// Common macros
+#define HIGH 	1
+#define LOW 	0
+#define TRUE	1
+#define FALSE	0
+
+#define RESET 0
+
+#endif /* PROJECT_H_ */
