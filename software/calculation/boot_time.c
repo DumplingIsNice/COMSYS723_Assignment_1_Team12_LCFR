debfe980dec7905c5c5417f5a8c72e5f9c3340bb
uoa-cnan194 <68796359+uoa-cnan194@users.noreply.github.com>
2022-04-18 16:02:32 +1200

    update boot_time.c

--- software/calculation/boot_time.c
+++ software/calculation/boot_time.c
@@ -4,6 +4,16 @@
 
 #include "boot_time.h"
 
+unsigned int millisec;
+char* system_uptime[10];
+char* boot_timing(); 
+
+char* boot_timing(){
+  millisec = xTaskGetTickCount();
+	sprintf(system_uptime, "%02d:%02d:%02d.%1d", (millisec/3600000) % 24, (millisec/60000) % 60, (millisec/1000) % 60, (millisec/100) % 10);
+  return system_uptime;
+}
+
 int main(){
 	while(1){
         printf("%s\n", boot_timing());   //print the system up time for testing
